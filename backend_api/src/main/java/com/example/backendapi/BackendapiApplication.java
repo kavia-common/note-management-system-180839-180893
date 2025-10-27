b3f6f06ee9539f1ae7fc91135a5c3ecea9f8ea2b
Kavia AI <production@kavia.ai>
2025-10-27 12:55:47 +0000

    CheckPoint - cg309081a6

--- backend_api/src/main/java/com/example/backendapi/BackendapiApplication.java
+++ backend_api/src/main/java/com/example/backendapi/BackendapiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class backendapiApplication {
+public class BackendapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(backendapiApplication.class, args);
+		SpringApplication.run(BackendapiApplication.class, args);
 	}
 
 }
